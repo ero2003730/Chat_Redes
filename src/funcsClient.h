32dbcc680df89f1648d2b908f1532c5fbfa85294
ero2003730 <96956416+ero2003730@users.noreply.github.com>
2023-09-11 16:27:51 -0300

    Update funcsClient.h

--- src/funcsClient.h
+++ src/funcsClient.h
@@ -10,7 +10,7 @@
 #include <sys/select.h>
 
 //Definindo o IP do server, a porta e o tamanho máximo da mensagem
-#define SERVER_IP "127.0.0.1"
+#define SERVER_IP "172.21.217.139"
 #define PORT 5005
 #define MAX_MSG_SIZE 1024
 
